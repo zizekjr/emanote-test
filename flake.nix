7d78f5443f1930781308a9d3cff39f033ed8010e
zizekjr <140620182+zizekjr@users.noreply.github.com>
2023-08-28 03:56:46 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
             layers = [ ./. ];
             layersString = [ "." ];
             # port = 8080;
-            baseUrl = "/emanote-test/"; # Change to "/" (or remove it entirely) if using CNAME
+            baseUrl = "/zjr.xyz/"; # Change to "/" (or remove it entirely) if using CNAME
             # prettyUrls = true;
           };
         };
